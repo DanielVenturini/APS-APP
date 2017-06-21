ea2440a13c5657b7045e70755978adf78cb894ce
Daniel Venturini <daniellagarto0@gmail.com>
2017-06-21 10:34:11 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,45 @@
-package main;
+package Principal;
 
 import java.io.FileNotFoundException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-import conexao.Conexao;
-import conexao.OperacaoSQL;
-import usuario.Usuario;
+import Conexao.ConexaoSQL;
+import Conexao.OperacaoSQL;
+import Funcionario.FuncionarioDAO;
+import Usuario.Extrato;
+import Usuario.Usuario;
+import Usuario.UsuarioDAO;
 
-public class Main {
-	
-	public static void main(String[] args) throws SQLException, FileNotFoundException{
+public class main {
+
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
+		OperacaoSQL.criarTabelas();
+		OperacaoSQL.criarCargos();
+		OperacaoSQL.criarFuncionarios();
+		OperacaoSQL.criarTipos();
+		OperacaoSQL.criarUsuarios();
+		OperacaoSQL.criarReajuste();
 		
-		//Conexao.getConection("postgres", "root", "apsapp");
-		//OperacaoSQL op = new OperacaoSQL();
-		Usuario u= new Usuario("1724487", "nome", "senha", "foto");
-		u.comprar(3);	
+		FuncionarioDAO f = new FuncionarioDAO();
+		UsuarioDAO u = new UsuarioDAO();
 		
+		if(f.autenticaFuncionario(1))
+			System.out.println("americamemes");
 		
+		if(u.autenticaUsuario(1724533, "gorvvq85"))
+			System.out.println("americamemes");
 		
+		f.vender(5, 1724533);
+		f.vender(3, 1724487);
 		
+		f.consumo(1724533);
+		f.consumo(1724533);
+		
+		f.consumo(1724487);
+		f.consumo(1724487);
+		f.consumo(1724487);
+		f.consumo(1724487);
 	}
-
 }
